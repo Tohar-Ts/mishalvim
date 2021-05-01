e0f2896cd0653150c54ccefddb50f9528d17d63a
ayeletGi <72082080+ayeletgi@users.noreply.github.com>
2021-05-01 18:09:59 +0300

    user class basic

--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,4 +1,39 @@
+
+import java.util.*;
+
 public class User {
-    String userName;
+
+    protected String userName;
+    protected String userID;
+    protected String userType;
+    protected String userEmail;
+    protected String userPassword;
+
+    /**
+     * @param name      The user full name - first and last.
+     * @param id        The user firebase id.
+     * @param type      The user type - admin, manager or volunteer.
+     * @param email     The user email - used for login.
+     * @param password  The user password - used for login.
+     */
+    public User(String name, String id, String type, String email, String password) {
+        userName = name;
+        userID = id;
+        userType = type;
+        userEmail = email;
+        userPassword = password;
+    }
+
+    public void setUserName(String newName) {
+        userName = newName;
+    }
+
+    public void setUserPassword(String newPassword) {
+       userPassword = newPassword;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
 
 }
