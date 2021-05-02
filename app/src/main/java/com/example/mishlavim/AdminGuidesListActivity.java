c2c63a0477c5dd844cf0c6422a714a489694bc7f
AdiMalachi4321 <adida901@gmail.com>
2021-05-02 21:39:30 +0300

    manager screens basic

---
+++ app/src/main/java/com/example/mishlavim/AdminGuidesListActivity.java
@@ -0,0 +1,14 @@
+package com.example.mishlavim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminGuidesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_guides_list);
+    }
+}
---
+++ app/src/main/java/com/example/mishlavim/CreateFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.mishlavim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_form);
+    }
+}
