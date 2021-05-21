b8bef75559b39d2a89a2ae9daae077c0484233b6
Elchanan Hattin <68992224+Ehattin@users.noreply.github.com>
2021-05-21 16:10:42 +0300

    Various Admin Activities
    Main Screen (toggle between forms and guides)
    everything is clicky

--- app/src/main/java/com/example/mishlavim/adminActivities/AdminMainActivity.java
+++ app/src/main/java/com/example/mishlavim/adminActivities/AdminMainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mishlavim.adminActivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mishlavim.R;
+import com.example.mishlavim.volunteerActivities.fillOutFormActivity;
+import com.example.mishlavim.volunteerActivities.viewOldFormActivity;
 
 public class AdminMainActivity extends AppCompatActivity {
 
@@ -12,5 +16,21 @@ public class AdminMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_admin);
+    }
+    public void AdminGuidesListActivity(View v) {
+        Intent i = new Intent(this, AdminGuidesListActivity.class);
+        startActivity(i);
+    }
+    public void AdminFormListActivity(View v){
+        Intent i = new Intent(this, AdminFormListActivity.class);
+        startActivity(i);
+    }
+    public void fillOutFormActivity(View v){
+        Intent i = new Intent(this, fillOutFormActivity.class);
+        startActivity(i);
+    }
+    public void viewOldFormActivity(View v){
+        Intent i = new Intent(this, viewOldFormActivity.class);
+        startActivity(i);
     }
 }
