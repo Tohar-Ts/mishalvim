ccb07935f9975b4a8aee2732a9148ada93200bd3
AdiMalachi4321 <adida901@gmail.com>
2021-05-26 11:22:58 +0300

    adding show/hide to password+validation

--- app/src/main/java/com/example/mishlavim/model/Validation.java
+++ app/src/main/java/com/example/mishlavim/model/Validation.java
@@ -28,64 +28,105 @@ public class Validation {
 
     public boolean validateInput() {
 
-        String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
-
-        if (!isEmailValid(email)) {
-            setError(emailEditText, R.string.invalid_email);
-            return true;
-        }
+       if(!isEmailValid()){
+           return true;
+       }
         if (userNameEditText != null) {
-            String userName = userNameEditText.getText().toString();
-            if (!isUserNameValid(userName)) {
-                setError(userNameEditText, R.string.invalid_userName);
+            if (!isUserNameValid()) {
                 return true;
             }
         }
-        if (!isPasswordValid(password)) {
-            setError(passwordEditText, R.string.invalid_password);
+
+        if (!isPasswordValid()) {
             return true;
         }
         if (verifyPasswordEditText != null) {
+
             String verifyPassword = verifyPasswordEditText.getText().toString().trim();
             if(verifyPassword.isEmpty()){
-                setError(verifyPasswordEditText, R.string.invalid_verifyPassword);
+                setError(verifyPasswordEditText, R.string.empty_verifyPassword);
                 return true;
             }
+
             if (!isVerifyPasswordValid(password, verifyPassword)) {
-                setError(verifyPasswordEditText, R.string.invalid_verifyPasswordSame);
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isEmailValid(String email) {
-        if (email == null) {
+
+    private boolean isEmailValid() {
+        String email = emailEditText.getText().toString().trim();
+        if (email.isEmpty()) {
+            setError(emailEditText,R.string.empty_email);
             return false;
         }
-        if (email.contains("@")) {
-            return Patterns.EMAIL_ADDRESS.matcher(email).matches();
-        } else
+        else if (email.contains("@")) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                setError(emailEditText, R.string.invalid_email);
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            setError(emailEditText,R.string.invalid_email);
             return false;
+        }
+
     }
 
-    private boolean isPasswordValid(String password) {
+
+    private boolean isPasswordValid() {
+        String password = passwordEditText.getText().toString().trim();
+
+        if(password.isEmpty()){
+                setError(passwordEditText, R.string.empty_password);
+                return false;
+            }
         int passwordLength = 5;
-        return (password != null && password.trim().length() > passwordLength);
+       if(password != null && password.trim().length() > passwordLength){
+           return true;
+       }
+       else{
+           setError(passwordEditText, R.string.invalid_password);
+           return false;
+       }
 
 
     }
-    private boolean isUserNameValid(String userName) {
-        if (userName == null) {
+    private boolean isUserNameValid() {
+        String fullName = userNameEditText.getText().toString();
+        if (fullName.isEmpty()) {
+            setError(userNameEditText, R.string.invalid_userName);
             return false;
         }
-        return userName.contains(" ");
+       else if (!fullName.contains(" ")){
+            setError(userNameEditText, R.string.invalid_userName);
+            return false;
+        }
+       else {
+           return true;
+        }
+
     }
 
 
     private boolean isVerifyPasswordValid(String password,String verifyPassword) {
-        return (password.equals(verifyPassword));
+        if(verifyPassword.isEmpty()){
+            setError(verifyPasswordEditText, R.string.empty_verifyPassword);
+            return false;
+        }
+        else if(!password.equals(verifyPassword)){
+            setError(verifyPasswordEditText, R.string.invalid_verifyPasswordSame);
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
     private void setError(EditText onEditText, Integer errorMsg) {
