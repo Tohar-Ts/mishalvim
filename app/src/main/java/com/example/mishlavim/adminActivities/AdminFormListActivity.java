f35ee66e8a46004b44a23706b19152e9a0b89abb
Elchanan Hattin <68992224+Ehattin@users.noreply.github.com>
2021-05-31 15:05:58 +0300

    return the finished screen activity

--- app/src/main/java/com/example/mishlavim/adminActivities/AdminFormListActivity.java
+++ app/src/main/java/com/example/mishlavim/adminActivities/AdminFormListActivity.java
@@ -7,6 +7,12 @@ import com.example.mishlavim.R;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AdminFormListActivity extends Fragment {
 
@@ -17,6 +23,32 @@ public class AdminFormListActivity extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.activity_admin_form_list, container, false);
+        ListView listView=(ListView)view.findViewById(R.id.listview);
+
+        //create ArrayList of String
+        final ArrayList<String> arrayList=new ArrayList<>();
+
+        //Add elements to arraylist
+        arrayList.add("OLAF");
+        arrayList.add("DANNY");
+        arrayList.add("DAVID");
+        arrayList.add("AYELET");
+        arrayList.add("TOHAR");
+        arrayList.add("ADI");
+        arrayList.add("ELCHANAN");
+        //Create Adapter
+        ArrayAdapter arrayAdapter=new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,arrayList);
+
+        //assign adapter to listview
+        listView.setAdapter(arrayAdapter);
+
+        //add listener to listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(),"clicked item:"+i+" "+arrayList.get(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
