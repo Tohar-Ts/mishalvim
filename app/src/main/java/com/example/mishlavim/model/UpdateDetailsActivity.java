8fdfb99131bd27c66de19058b0cba04d074fa469
DavidShandor <davidshandor15@gmail.com>
2021-06-04 17:43:46 +0300

    user update details screen

---
+++ app/src/main/java/com/example/mishlavim/model/UpdateDetailsActivity.java
@@ -0,0 +1,53 @@
+package com.example.mishlavim.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.mishlavim.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class UpdateDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView username;
+    private EditText name, email, password,password_confirm ,phone;
+    private Validation validation;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_details);
+
+        username = findViewById(R.id.user_name);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password =findViewById(R.id.password);
+        password_confirm = findViewById(R.id.password_confirm);
+        phone = findViewById(R.id.phone);
+        Button saveBTM = findViewById(R.id.updateBTM);
+
+//        validation = new Validation(emailEditText, null, passwordEditText, null,
+//                loadingProgressBar, getResources());
+
+
+        saveBTM.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {updateUserDetails();}
+
+    private void updateUserDetails(){
+
+    }
+}
