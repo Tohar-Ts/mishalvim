07713abc12e8ed8ed9405865ffc5a33f66749e11
ayeletGi <ayelet47654@gmail.com>
2021-06-06 13:21:45 +0300

    guide give permission to volu

--- app/src/main/java/com/example/mishlavim/model/Firebase/FirestoreMethods.java
+++ app/src/main/java/com/example/mishlavim/model/Firebase/FirestoreMethods.java
@@ -245,8 +245,7 @@ public class FirestoreMethods {
 
 
 
-    public static void getCollection(String collection, Function<QuerySnapshot,
-            Void> successFunc,  Function<Void, Void> failedFunc){
+    public static void getCollection(String collection, Function<QuerySnapshot, Void> successFunc,  Function<Void, Void> failedFunc){
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
