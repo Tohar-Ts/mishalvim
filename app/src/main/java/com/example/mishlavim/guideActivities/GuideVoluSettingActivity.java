0b72abde68cbd986088cc8b2030848837235aea9
Elchanan Hattin <68992224+Ehattin@users.noreply.github.com>
2021-06-06 03:55:17 +0300

    Revert "Merge branch 'main' of https://github.com/Tohar-Ts/mishalvim into main"
    This reverts commit 65fa34ae271bb3d20050aa7e54f9417c6dbcfb4b, reversing
    changes made to aa0ce827f0b434ce4e52c5b11e8fa2ab9dc13479.

--- app/src/main/java/com/example/mishlavim/guideActivities/GuideVoluSettingActivity.java
+++ app/src/main/java/com/example/mishlavim/guideActivities/GuideVoluSettingActivity.java
@@ -3,135 +3,28 @@ package com.example.mishlavim.guideActivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.mishlavim.R;
-import com.example.mishlavim.login.Validation;
-import com.example.mishlavim.model.Admin;
-import com.example.mishlavim.model.Firebase.FirebaseStrings;
-import com.example.mishlavim.model.Firebase.FirestoreMethods;
-import com.example.mishlavim.model.Global;
-import com.example.mishlavim.model.Volunteer;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.security.acl.Group;
-import java.util.ArrayList;
-import java.util.HashMap;
+public class GuideVoluSettingActivity extends AppCompatActivity {
 
-
-public class GuideVoluSettingActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
-
-    private TextView userName, guideName;
-    private EditText name, email, password,password_confirm ;
-    private Validation validation;
-    private FirebaseAuth mAuth;
-    private FirebaseUser firebaseUser;
-    private FirebaseFirestore db;
     private String voluToUpdateName, voluToUpdateId;
-    private Spinner guidesSpinner;
-    private ArrayList<String> listOfGuidesName,  listOfGuidesID;
-    private Volunteer volunteer;
-    private Group group;
-
-
+    private TextView voluName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_details);
+        setContentView(R.layout.activity_guide_volu_setting);
+
+        //innit xml views
+        voluName = findViewById(R.id.edit_volu_name);
+
 
         //getting clicked volunteer details
         voluToUpdateName =  getIntent().getStringExtra("CLICKED_VOLU_KEY");
         voluToUpdateId =  getIntent().getStringExtra("CLICKED_VOLU_ID");
 
-        //get the volunteer document.
-        FirestoreMethods.getDocument(FirebaseStrings.usersStr(),voluToUpdateId,this::onGetDocSuccess,this::onGetDocFailed);
+        voluName.setText(voluToUpdateName);
 
-        Global globalInstance = Global.getGlobalInstance();//current user.
-
-        if (globalInstance.getType().equals(FirebaseStrings.adminStr())){
-            guidesSpinner = findViewById(R.id.spinner);
-            guideName = findViewById(R.id.guideName);
-            setSpinner();
-            guidesSpinner.setVisibility(View.VISIBLE);
-            guideName.setVisibility(View.VISIBLE);
-        }
-
-        group = findViewById(R.id.group2);
-        userName = findViewById(R.id.edit_volu_name);
-        userName.setText(voluToUpdateName);
-        name = findViewById(R.id.name);
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        password_confirm = findViewById(R.id.password_confirm);
-        Button saveBTM = findViewById(R.id.updateBTM);
-
-
-//        validation = new Validation(emailEditText, null, passwordEditText, null,
-//                loadingProgressBar, getResources());
-
-        saveBTM.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {updateUserDetails();}
-
-    private void updateUserDetails(){
-        // TODO: 6/6/2021 use validation before update.
-
-        FirestoreMethods.updateDocumentField(FirebaseStrings.usersStr(),voluToUpdateId,FirebaseStrings.nameStr(),name.getText(),task ->{
-            FirestoreMethods.updateDocumentField(FirebaseStrings.usersStr(),voluToUpdateId,FirebaseStrings.emailStr(),name.getText(),task2 ->{
-                    reloadScreen();
-                return  null;},this::onGetDocFailed);
-        return  null;},this::onGetDocFailed);
-
-    }
-
-    public void setSpinner(){
-        //SPINNER SETUP
-        //get the guides list.
-        Global globalInstance = Global.getGlobalInstance();
-        Admin admin = globalInstance.getAdminInstance();
-        HashMap<String,String> guideList = admin.getGuideList();
-
-        listOfGuidesName = new ArrayList<>(guideList.keySet());
-        listOfGuidesID = new ArrayList<>(guideList.values());
-
-        //Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,android.R.layout.simple_spinner_dropdown_item,listOfGuidesName);
-
-        guidesSpinner.setAdapter(adapter);
-        guidesSpinner.setPrompt(volunteer.getMyGuide());
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String key = listOfGuidesName.get(position);
-        String value = listOfGuidesID.get(position);
-        FirestoreMethods.moveVolunteer(voluToUpdateId,volunteer.getMyGuideId(),key,value);
-        reloadScreen();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {}
-
-    public Void onGetDocSuccess(DocumentSnapshot documentSnapshot){
-        volunteer = documentSnapshot.toObject(Volunteer.class);
-        return null;
-    }
-    public Void onGetDocFailed(Void noUse){return null;}
-
-    private void reloadScreen() {
-        finish();
-        startActivity(getIntent());
     }
 }
--- app/src/main/java/com/example/mishlavim/model/Firebase/FirebaseStrings.java
+++ app/src/main/java/com/example/mishlavim/model/Firebase/FirebaseStrings.java
@@ -13,11 +13,9 @@ public class FirebaseStrings {
         return "admin";
     }
 
-    public static String guideStr() { return "guide"; }
-
-    public static String myGuideStr() { return "myGuide"; }
-
-    public static String myGuideIdStr() { return "myGuideID"; }
+    public static String guideStr() {
+        return "guide";
+    }
 
     public static String volunteerStr() {
         return "volunteer";
@@ -29,10 +27,6 @@ public class FirebaseStrings {
 
     public static String typeStr() {
         return "type";
-    }
-
-    public static String emailStr() {
-        return "email";
     }
 
     public static String nameStr() {
