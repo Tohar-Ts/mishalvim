bcaa19f3cc1cc4193ec53afaaf957c26fbdcb598
ayeletGi <ayelet47654@gmail.com>
2021-06-07 12:03:51 +0300

    be careful this commit is not working!

---
+++ app/src/main/java/com/example/mishlavim/adminActivities/BottomNavigation.java
@@ -0,0 +1,14 @@
+package com.example.mishlavim.adminActivities;
+import com.example.mishlavim.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttom_navigation);
+    }
+}
