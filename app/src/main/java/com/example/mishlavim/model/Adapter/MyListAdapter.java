76a9a780e87185e1cb855b3eec56c919155654bb
Elchanan Hattin <68992224+Ehattin@users.noreply.github.com>
2021-06-08 22:31:14 +0300

    Made new fragment activities for all of the guide screens, also some small changed to Adapter and guide main
    lots of commented out functions because the fragments are full of conflicts

--- app/src/main/java/com/example/mishlavim/model/Adapter/MyListAdapter.java
+++ app/src/main/java/com/example/mishlavim/model/Adapter/MyListAdapter.java
@@ -1,9 +1,11 @@
 package com.example.mishlavim.model.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,8 +18,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
 import com.example.mishlavim.R;
+import com.example.mishlavim.dialogs.DeleteUserDialog;
+import com.example.mishlavim.guideActivities.GuideFormsPermissionActivity;
+import com.example.mishlavim.guideActivities.GuideVoluSettingActivity;
+import com.example.mishlavim.guideActivities.GuideMainActivity;
+import com.example.mishlavim.model.Firebase.FirebaseStrings;
+import com.example.mishlavim.model.Firebase.FirestoreMethods;
 
 import java.util.List;
 
@@ -45,21 +54,22 @@ public class MyListAdapter extends ArrayAdapter<String> implements Filterable {
             convertView.setTag(viewHolder);
         }
         mainViewHolder = (ViewHolder) convertView.getTag();
-        mainViewHolder.imagebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //init clicked id
-
-                clickedRowName = (String) v.getTag();
-                //showing the popup menu
-                PopupMenu popup = new PopupMenu(context, v);
-
-                MenuInflater inflater = popup.getMenuInflater();
-                inflater.inflate(R.menu.volunteer_options_menu, popup.getMenu());
-                popup.show();
-                Log.d("TAG", "onClick: ");
-            }
-        });
+        mainViewHolder.imagebutton.setOnClickListener(new myClickListener(getItemId(position)));
+//        mainViewHolder.imagebutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //init clicked id
+//
+//                clickedRowName = (String) v.getTag();
+//                //showing the popup menu
+//                PopupMenu popup = new PopupMenu(context, v);
+//
+//                MenuInflater inflater = popup.getMenuInflater();
+//                inflater.inflate(R.menu.volunteer_options_menu, popup.getMenu());
+//                popup.show();
+//                Log.d("TAG", "onClick: ");
+//            }
+//        });
 
 
 //            mainViewHolder.imagebutton.setOnClickListener(new View.OnClickListener() {
@@ -79,5 +89,61 @@ public class MyListAdapter extends ArrayAdapter<String> implements Filterable {
     @Override
     public Filter getFilter() {
         return super.getFilter();
+    }
+
+
+    public class myClickListener implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+
+        long id;
+
+        public myClickListener(long id) {
+            this.id = id;
+        }
+        @Override
+        public void onClick(View v) {
+            PopupMenu popup = new PopupMenu(context, v);
+            popup.setOnMenuItemClickListener(this);
+            MenuInflater inflater = popup.getMenuInflater();
+            inflater.inflate(R.menu.volunteer_options_menu, popup.getMenu());
+            popup.show();
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+//            //delete volunteer
+//            if (item.getItemId() == R.id.remove_volunteer) {
+//                DialogFragment newFragment = new DeleteUserDialog();
+//                newFragment.show(getSupportFragmentManager(), "deleteUser");
+//                return true;
+//            }
+//            else if (item.getItemId() == R.id.view_volunteer) {
+//                FirestoreMethods.getDocument(FirebaseStrings.usersStr(),  guide.getMyVolunteers().get(clickedRowName), this::getUserDocSuccess, this::getUserDocFailed);
+//                Log.d("clicked:", clickedRowName + " view" );
+//                return true;
+//            }
+//
+//            else if (item.getItemId() == R.id.edit_volunteer) {
+//                Intent intent = new Intent(getApplicationContext(), GuideVoluSettingActivity.class);
+//                intent.putExtra("CLICKED_VOLU_KEY", clickedRowName);
+//                intent.putExtra("CLICKED_VOLU_ID", guide.getMyVolunteers().get(clickedRowName));
+//                startActivity(intent);
+//                overridePendingTransition(0, 0);
+//                return true;
+//            }
+//            else if (item.getItemId() == R.id.open_form_to_volunteer) {
+//                Intent intent = new Intent(getApplicationContext(), GuideFormsPermissionActivity.class);
+//                intent.putExtra("CLICKED_VOLU_KEY", clickedRowName);
+//                intent.putExtra("CLICKED_VOLU_ID", guide.getMyVolunteers().get(clickedRowName));
+//                startActivity(intent);
+//                overridePendingTransition(0, 0);
+//                return true;
+//            }
+//            else if (item.getItemId() == R.id.view_volunteer) {
+//                FirestoreMethods.getDocument(FirebaseStrings.usersStr(), guide.getMyVolunteers().get(clickedRowName), this::getUserDocSuccess, this::getUserDocFailed);
+//                Log.d("clicked:", clickedRowName + " view" );
+//                return true;
+//            }
+            return false;
+        }
     }
 }
