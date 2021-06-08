e031ee45321d7faf6f523662ae68ca91fea57687
tohar <toharbk@gmail.com>
2021-06-08 16:45:11 +0300

    shoe guide popup menu

--- app/src/main/java/com/example/mishlavim/model/Adapter/MyListAdapter.java
+++ app/src/main/java/com/example/mishlavim/model/Adapter/MyListAdapter.java
@@ -3,6 +3,7 @@ package com.example.mishlavim.model.Adapter;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -10,7 +11,9 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -21,14 +24,17 @@ import java.util.List;
 public class MyListAdapter extends ArrayAdapter<String> implements Filterable {
     private int layout;
     private List<String> mObjects;
+    private Context context;
+    private String clickedRowName;
     public MyListAdapter(Context context, int resource, List<String> objects) {
         super(context, resource, objects);
+        this.context = context;
         mObjects = objects;
         layout = resource;
     }
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        ViewHolder mainViewholder = null;
+        ViewHolder mainViewHolder = null;
         if(convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(layout, parent, false);
@@ -38,31 +44,37 @@ public class MyListAdapter extends ArrayAdapter<String> implements Filterable {
             viewHolder.imagebutton = (ImageButton) convertView.findViewById(R.id.list_item_btn);
             convertView.setTag(viewHolder);
         }
-        mainViewholder = (ViewHolder) convertView.getTag();
-        mainViewholder.imagebutton.setOnClickListener(new View.OnClickListener() {
+        mainViewHolder = (ViewHolder) convertView.getTag();
+        mainViewHolder.imagebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //init clicked id
-//                clickedRowName = (String) v.getTag();
-//                //showing the popup menu
-//                PopupMenu popup = new PopupMenu(getBaseContext(), v);
-//                MenuInflater inflater = popup.getMenuInflater();
-//                inflater.inflate(R.menu.volunteer_options_menu, popup.getMenu());
-//                popup.show();
+
+                clickedRowName = (String) v.getTag();
+                //showing the popup menu
+                PopupMenu popup = new PopupMenu(context, v);
+
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.volunteer_options_menu, popup.getMenu());
+                popup.show();
                 Log.d("TAG", "onClick: ");
             }
         });
-//            mainViewholder.imagebutton.setOnClickListener(new View.OnClickListener() {
+
+
+//            mainViewHolder.imagebutton.setOnClickListener(new View.OnClickListener() {
 //                @Override
 //                public void onClick(View v) {
 //                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
 //                }
 //            });
-        mainViewholder.title.setText(getItem(position));
+        mainViewHolder.title.setText(getItem(position));
 
         return convertView;
     }
-
+//    public void setContext (Context con){
+//        this.context = con;
+//    }
     @NonNull
     @Override
     public Filter getFilter() {
