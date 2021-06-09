1d19a96b4122e2e4fec8cda4973e26cc140e08e3
ayeletGi <ayelet47654@gmail.com>
2021-06-09 20:41:57 +0300

    background color update to gray

--- app/src/main/java/com/example/mishlavim/guideActivities/GuideNavigationActivity.java
+++ app/src/main/java/com/example/mishlavim/guideActivities/GuideNavigationActivity.java
@@ -2,15 +2,20 @@ package com.example.mishlavim.guideActivities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.mishlavim.R;
 import com.example.mishlavim.adminActivities.AdminAddNewUserFragment;
+import com.example.mishlavim.adminActivities.AdminNavigationActivity;
+import com.example.mishlavim.login.LoginActivity;
+import com.example.mishlavim.model.Firebase.AuthenticationMethods;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
@@ -23,14 +28,23 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 public class GuideNavigationActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_navigation);
+        //setting bar
+        Toolbar settingBar = findViewById(R.id.guide_setting_button);
+        setSupportActionBar(settingBar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(null);
+
         //navigation bar listener
         BottomNavigationView navigationBtn = findViewById(R.id.guide_bottom_navigation);
         navigationBtn.setOnNavigationItemSelectedListener(this);
@@ -80,6 +94,24 @@ public class GuideNavigationActivity extends AppCompatActivity implements Bottom
         return true;
     }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.setting:
+                //TODO navigate to setting
+                Toast.makeText(GuideNavigationActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit:
+                AuthenticationMethods.signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                overridePendingTransition(0, 0);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
