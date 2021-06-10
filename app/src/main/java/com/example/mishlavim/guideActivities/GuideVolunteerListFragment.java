0be113293fec6d49cce2f9982395b91cf634f49a
ayeletGi <ayelet47654@gmail.com>
2021-06-10 07:58:40 +0300

    guide volunteer list update TODO

--- app/src/main/java/com/example/mishlavim/guideActivities/GuideVolunteerListFragment.java
+++ app/src/main/java/com/example/mishlavim/guideActivities/GuideVolunteerListFragment.java
@@ -14,58 +14,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupMenu;
-
 import com.example.mishlavim.R;
 import com.example.mishlavim.dialogs.DeleteUserDialog;
 import com.example.mishlavim.model.Adapter.RecyclerAdapter;
-import com.example.mishlavim.model.Admin;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import org.jetbrains.annotations.NotNull;
-
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.ContextThemeWrapper;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.PopupMenu;
-import android.widget.TableLayout;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.mishlavim.R;
-import com.example.mishlavim.dialogs.DeleteUserDialog;
-import com.example.mishlavim.login.LoginActivity;
-import com.example.mishlavim.model.Adapter.MyListAdapter;
-import com.example.mishlavim.model.Firebase.AuthenticationMethods;
 import com.example.mishlavim.model.Firebase.FirebaseStrings;
 import com.example.mishlavim.model.Firebase.FirestoreMethods;
 import com.example.mishlavim.model.Global;
 import com.example.mishlavim.model.Guide;
 import com.example.mishlavim.model.Volunteer;
 import com.example.mishlavim.volunteerActivities.VolunteerMainActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.DocumentSnapshot;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-public class GuideVolunteerListFragment extends Fragment implements PopupMenu.OnMenuItemClickListener, DeleteUserDialog.deleteUserListener, SearchView.OnQueryTextListener {
+public class GuideVolunteerListFragment extends Fragment implements PopupMenu.OnMenuItemClickListener{
 
     RecyclerView guidesView;
     RecyclerAdapter recyclerAdapter;
@@ -120,12 +86,14 @@ public class GuideVolunteerListFragment extends Fragment implements PopupMenu.On
 
         switch(item.getItemId() ){
             case R.id.view_volunteer:
+                //getting the volunteer data and moving to volunteer main
                 FirestoreMethods.getDocument(FirebaseStrings.usersStr(), clickedRowUid , this::getUserDocSuccess, this::getUserDocFailed);
                 break;
             case R.id.edit_volunteer:
-                //TODO
+                //TODO - show what is the volunteer name?.
                 break;
             case R.id.open_form_to_volunteer:
+                //moving to the activity, passing the clicked volunteer details
                 Intent intent = new Intent(getActivity().getBaseContext(),
                         GuideFormsPermissionActivity.class);
                 intent.putExtra("CLICKED_VOLU_KEY", clickedRowText);
@@ -133,40 +101,20 @@ public class GuideVolunteerListFragment extends Fragment implements PopupMenu.On
                 getActivity().startActivity(intent);
                 break;
             case R.id.remove_volunteer:
-                DialogFragment newFragment = new DeleteUserDialog();
-                newFragment.show(getActivity().getSupportFragmentManager(), "deleteUser");
+               //TODO - show yes/no dialog.
                 break;
         }
         return true;
     }
 
     //search functions
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        Context context = getContext();
-        //here im checking to see if the search is working upon submit
-        Toast.makeText(context,"Our word : "+query,Toast.LENGTH_SHORT).show();
-        return false;
-    }
+    //TODO - fixing search to recycle view
 
-
-    @Override
-    //when user type in searchview get string as newText parameter
-    public boolean onQueryTextChange(String newText) {
-//        recyclerAdapter.getFilter().filter(newText);
-        return false;
-    }
     //delete dialog functions
-    @Override
     public void onDeletePositiveClick(DialogFragment dialog) {
         Guide.deleteVolunteer(global.getUid(), clickedRowUid);
         FirestoreMethods.deleteDocument(FirebaseStrings.usersStr(), clickedRowUid, this::onDocumentDeleteSuccess, this::onDeleteFailed);
     }
-
-    @Override
-    public void onDeleteNegativeClick(DialogFragment dialog) {
-    }
-
 
     public Void onDocumentDeleteSuccess(Void noUse){
         Toast.makeText(getActivity(), "המחיקה הסתיימה בהצלחה", Toast.LENGTH_SHORT).show();
@@ -178,8 +126,8 @@ public class GuideVolunteerListFragment extends Fragment implements PopupMenu.On
         Toast.makeText(getActivity(), "מחיקה נכשלה", Toast.LENGTH_SHORT).show();
         return null;
     }
-    //go to watch volunteer as a guide
 
+    //go to watch volunteer as a guide
     private Void getUserDocSuccess(DocumentSnapshot doc){
         assert doc != null;
         Volunteer volu = doc.toObject(Volunteer.class);
